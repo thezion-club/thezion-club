75b347866cacd0b37f1d8f5b966494be0b30a37d
thezion-club <admin@thezion.club>
2024-12-06 15:29:56 +0800

    Merge thezion-dev (#6)
    * Merge linusxiong/Xboard Stripe Payment Method (#2)
    
    * [update] 新增stripe聚合支付方式，采用全新的paymentIntents API
    [fix] 修改支付方式中的小bug
    [update] 将stripe-php版本升级至最新
    
    * Update StripeALLInOne.php
    
    修复部分报错语句不规范
    
    * Update StripeALLInOne.php
    
    删除调试冗余代码
    
    * Fix PaymentController.php
    
    修复由Jun 8, 2024提交的2个commit产生的报错
    
    * [fix] 修复catch的时候变量可能undefined的问题
    
    * [feat] 新增telegram机器人/start指令
    
    * [fix] 删除重复包导入
    
    * [update] 新增credit card跳转至checkout页面付款
    
    * Update Surge.php
    
    support surge ss2022
    
    ---------
    
    Authored-by: LinusX <siyuanxiong2333@gmail.com>
    
    * Modify for thezion-club environment
    
    * Update clash config
    
    * Remove Clash.php, amend ClashMeta flags
    
    * Merge thezion-dev (#3)
    
    * Update clash config
    
    * Remove Clash.php, amend ClashMeta flags
    
    * Update workflow, refine clash rules
    
    * Fork Xboard theme, fix Shadowrocket translation issue.
    
    * Fix Shadowrocket translation
    
    * Reformat umi.js for readibility
    
    * Remove unused languages, refine translation.
    
    * Fix translation
    
    * Fix format
    
    * Fix translation
    
    * Fix translation (compress umi.js)
    
    * Try to fix last update issue
    
    I really need to learn coding...
    
    * Update configs
    
    * Update nameserver

---
+++ public/theme/thezion/dashboard.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="zh-CN">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
+  <title>{{$title}}</title>
+  <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js"></script>
+</head>
+
+<body>
+
+  <script>
+    window.routerBase = "/";
+    window.settings = {
+      title: '{{$title}}',
+      assets_path: '/theme/{{$theme}}/assets',
+      theme: {
+        color: '{{ $theme_config['theme_color'] ?? "default" }}',
+      },
+      version: '{{$version}}',
+      background_url: '{{$theme_config['background_url']}}',
+      description: '{{$description}}',
+      i18n: [
+        'zh-CN',
+        'en-US',
+      ],
+      logo: '{{$logo}}'
+    }
+  </script>
+  <div id="app"></div>
+  {!! $theme_config['custom_html'] !!}
+</body>
+
+</html>
